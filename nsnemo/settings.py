9b92841c3fcd182eb8a042bb9adc8854e39bb8db
jalal1985khan <jalal1985khan@gmail.com>
2023-09-25 14:48:05 +0530

    new code for nemo

--- nsnemo/settings.py
+++ nsnemo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w*-*9&+69^x(unfhrc9*^e2)q7u+_0qj#qhx#q$jh0+l7!(&%b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django.ivistasolutions.biz','127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['django.ivistasolutions.biz','127.0.0.1','.vercel.app','.now.sh']
 CSRF_TRUSTED_ORIGINS = ['https://django.ivistasolutions.biz']
 
 
@@ -131,12 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-#STATICFILES_DIRS = [
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 #    os.path.join(BASE_DIR, 'static')
 #]
 
 #BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
