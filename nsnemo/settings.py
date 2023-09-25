6e840c1edfe7b13b4eda84b7323cde7a9d065b60
jalal1985khan <jalal1985khan@gmail.com>
2023-09-26 00:21:30 +0530

    1

--- nsnemo/settings.py
+++ nsnemo/settings.py
@@ -130,12 +130,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
+#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 #    os.path.join(BASE_DIR, 'static')
 #]
 
 #BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
