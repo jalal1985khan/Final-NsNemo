8115c87fb96a9bc5c8a374413f1051dd6f1696bb
jalal1985khan <jalal1985khan@gmail.com>
2023-10-11 15:51:26 +0530

    11-oct-changes on media settings

--- nsnemo/settings.py
+++ nsnemo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w*-*9&+69^x(unfhrc9*^e2)q7u+_0qj#qhx#q$jh0+l7!(&%b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://django.ivistasolutions.biz']
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'nsnemo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['staticfiles','templates'],
+        'DIRS': [BASE_DIR,"templates", "staticfiles"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'nsnemo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -130,23 +130,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
+MEDIA_URL ='/media/'
 #STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #    os.path.join(BASE_DIR, 'static')
 #]
 
 #BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
-MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#STATIC_ROOT = BASE_DIR/ "static/"
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# STATICFILES_DIRS = [
+#     BASE_DIR/ "staticfiles",
+    
+# ]
+MEDIA_ROOT = BASE_DIR/"staticfiles/images"
+STATIC_ROOT = BASE_DIR/ "staticfiles/"
 
 LOGGING = {
     "version": 1,
